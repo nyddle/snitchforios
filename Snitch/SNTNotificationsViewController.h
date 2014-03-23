4a826cd21898ef03e79321d7012d5b72dcf198d9
Alexander Davydov <nyddle@gmail.com>
2014-03-23 15:09:02 +0400

    add notifications view controller

---
+++ Snitch/SNTNotificationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SNTNotificationsViewController.h
+//  Snitch
+//
+//  Created by Alexander Davydov on 23.03.14.
+//  Copyright (c) 2014 Alexander Davydov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SNTNotificationsViewController : UITableViewController
+
+@end
