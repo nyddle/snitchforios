1fca9c3367a260e17c05bd37254dd2366b37f856
Alexander Davydov <nyddle@gmail.com>
2014-03-23 14:07:27 +0400

    initial commit

---
+++ SNTNotification.m
@@ -0,0 +1,13 @@
+//
+//  SNTNotification.m
+//  Snitch
+//
+//  Created by Alexander Davydov on 23.03.14.
+//  Copyright (c) 2014 Alexander Davydov. All rights reserved.
+//
+
+#import "SNTNotification.h"
+
+@implementation SNTNotification
+
+@end
