48b02064e602813fc3b5e9939db3cf0de75e1ed3
Alexander Davydov <nyddle@gmail.com>
2014-03-23 14:46:19 +0400

    add notification properties

--- SNTNotification.h
+++ SNTNotification.h
@@ -6,8 +6,28 @@
 //  Copyright (c) 2014 Alexander Davydov. All rights reserved.
 //
 
+/*
+ {
+ "app" : 'Snitch',
+ "title" : "Stack Overflow",
+ "text" : "trace message",
+ type : "alert",
+ time : 10000
+ }
+ */
+
+
 #import <Foundation/Foundation.h>
 
 @interface SNTNotification : NSObject
+
+@property (nonatomic, copy) NSString *appName;
+@property (nonatomic, copy) NSString *eventName;
+@property (nonatomic, copy) NSString *eventText;
+@property (nonatomic, copy) NSString *eventType;
+
+@property (nonatomic, readonly, strong) NSDate *dateCreated;
+
+
 
 @end
--- SNTNotification.m
+++ SNTNotification.m
@@ -4,7 +4,17 @@
 //
 //  Created by Alexander Davydov on 23.03.14.
 //  Copyright (c) 2014 Alexander Davydov. All rights reserved.
-//  1
+//
+
+/*
+{
+ "app" : 'Snitch',
+ "title" : "Stack Overflow",
+ "text" : "trace message",
+ type : "alert",
+ time : 10000
+}
+*/
 
 #import "SNTNotification.h"
 
